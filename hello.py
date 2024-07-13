1d3306b1ffef0dfc5a9119e7562bfcc81b18d873
jacobo_mejia <jacobomejia2007@gmail.com>
2024-07-13 11:53:29 -0400

    Adding the option of substraction

--- hello.py
+++ hello.py
@@ -1,2 +1,9 @@
-x, y = input("Addition: ").split("+")
-print(int(x) + int(y))
+operation = input("Operation(addition/substraction): ")
+
+if operation == "addition":
+    x, y = input("addition: ").split("+")
+    print(int(x) + int(y))
+
+else:
+    x, y = input("substraction: ").split("-")
+    print(int(x) - int(y))
